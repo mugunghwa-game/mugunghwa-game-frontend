26293eb035a8b605ae6efc97da2dc6edb39c80ee
rami0617 <aarmakrr@hanmail.net>
2022-07-14 22:10:17 +0900

    [CHORE]: Change react-webcam to video

--- src/components/Video.js
+++ src/components/Video.js
@@ -6,8 +6,6 @@ import useStore from "../store/store";
 export default function Video(props) {
   const anotherUserRef = useRef();
 
-  //   console.log(props.peer.streams[0].id, "props");
-
   useEffect(() => {
     props.peer.on("stream", (stream) => {
       anotherUserRef.current.srcObject = stream;
