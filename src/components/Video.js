f0a75cb0ef6a44bf7d0ad3c1001af7144d8c88e7
rami0617 <aarmakrr@hanmail.net>
2022-07-14 21:27:32 +0900

    [FIX]: Fixing Bug

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,17 +2,14 @@ import React, { useEffect, useRef, useState } from "react";
 import Webcam from "react-webcam";
 
 import useStore from "../store/store";
-export default function Video(props) {
-  const { addVideoNumber, vidoeNumber, firstVideo } = useStore();
 
+export default function Video(props) {
   const anotherUserRef = useRef();
 
-  console.log(props.peer.streams[0].id, "props");
+  //   console.log(props.peer.streams[0].id, "props");
 
   useEffect(() => {
     props.peer.on("stream", (stream) => {
-      console.log("다른 사람stream", stream);
-      console.log("hereis");
       anotherUserRef.current.srcObject = stream;
       console.log(anotherUserRef);
     });
